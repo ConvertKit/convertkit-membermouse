694defc4c0c0ee991c8941b6aad066487020db15
Tim Carr <tim@n7studios.co.uk>
2024-06-26 16:46:51 +0800

    Added tests for the settings screen; started tag tests

---
+++ tests/acceptance/general/MemberTagCest.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Tests that subscribers are added to ConvertKit and tagged
+ * based on the membership level.
+ *
+ * @since   1.2.0
+ */
+class MemberTagCest
+{
+	/**
+	 * Run common actions before running the test functions in this class.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function _before(AcceptanceTester $I)
+	{
+		// Activate Plugins.
+		$I->activateConvertKitPlugin($I);
+		$I->activateThirdPartyPlugin($I, 'membermouse-platform');
+	}
+
+	/**
+	 * Test that saving settings on the settings screen with no changes
+	 * works with no errors.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testUserSubscribedAndTagged(AcceptanceTester $I)
+	{
+		// Setup Plugin to tag users added to the Free Membership level to the
+		// ConvertKit Tag ID.
+		$I->setupConvertKitPlugin(
+			$I,
+			[
+				'convertkit-mapping-1' => $_ENV['CONVERTKIT_API_TAG_ID'],
+			]
+		);
+
+		// Generate email address for test.
+		$emailAddress = $I->generateEmailAddress();
+
+		// Navigate to MemberMouse > Manage Members.
+		$I->amOnAdminPage('admin.php?page=manage_members');
+
+		// Create Member.
+		$I->click('Create Member');
+		$I->waitForElementVisible('#mm-new-member-form-container');
+		$I->fillField('#mm-new-first-name', 'First');
+		$I->fillField('#mm-new-last-name', 'Last');
+		$I->fillField('#mm-new-email', $emailAddress);
+		$I->fillField('#mm-new-password', '12345678');
+		$I->click('Create Member');
+
+		// Accept popup once user created.
+		$I->acceptPopup();
+
+		// Check that the subscriber has been assigned to the tag.
+		$I->apiCheckSubscriberHasTag($I, $subscriberID, $_ENV['CONVERTKIT_API_TAG_ID']);
+	}
+
+	/**
+	 * Deactivate and reset Plugin(s) after each test, if the test passes.
+	 * We don't use _after, as this would provide a screenshot of the Plugin
+	 * deactivation and not the true test error.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function _passed(AcceptanceTester $I)
+	{
+		$I->deactivateThirdPartyPlugin($I, 'membermouse-platform');
+		$I->deactivateConvertKitPlugin($I);
+		$I->resetConvertKitPlugin($I);
+	}
+}
---
+++ tests/acceptance/general/SettingsCest.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Tests the Plugin settings screen.
+ *
+ * @since   1.2.0
+ */
+class SettingsCest
+{
+	/**
+	 * Run common actions before running the test functions in this class.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function _before(AcceptanceTester $I)
+	{
+		// Activate Plugins.
+		$I->activateConvertKitPlugin($I);
+		$I->activateThirdPartyPlugin($I, 'membermouse-platform');
+	}
+
+	/**
+	 * Test that saving settings on the settings screen with no changes
+	 * works with no errors.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testSaveSettingsWithNoChanges(AcceptanceTester $I)
+	{
+		// Go to the Plugin's Settings > General Screen.
+		$I->amOnAdminPage('options-general.php?page=convertkit-mm');
+
+		// Click save settings.
+		$I->click('Save Settings');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+	}
+
+	/**
+	 * Test that saving a valid API Key on the settings screen
+	 * works with no errors.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testSaveValidAPIKey(AcceptanceTester $I)
+	{
+		// Go to the Plugin's Settings > General Screen.
+		$I->amOnAdminPage('options-general.php?page=convertkit-mm');
+
+		// Complete API Field.
+		$I->fillField('convertkit-mm-options[api-key]', $_ENV['CONVERTKIT_API_KEY']);
+
+		// Click save settings.
+		$I->click('Save Settings');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+
+		// Confirm settings saved.
+		$I->see('Settings saved.');
+	}
+
+	/**
+	 * Test that saving an invalid API Key on the settings screen
+	 * works with no errors.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testSaveInvalidAPIKey(AcceptanceTester $I)
+	{
+		// Go to the Plugin's Settings > General Screen.
+		$I->amOnAdminPage('options-general.php?page=convertkit-mm');
+
+		// Complete API Field.
+		$I->fillField('convertkit-mm-options[api-key]', 'fakeApiKey');
+
+		// Click save settings.
+		$I->click('Save Settings');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+
+		// Confirm settings saved.
+		// The Plugin doesn't show an error if an invalid API Key is present; in the future
+		// we'll want to add a notice in the Plugin and then test for it here.
+		$I->see('Settings saved.');
+	}
+
+	/**
+	 * Test that saving an invalid API Key on the settings screen
+	 * works with no errors.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testSaveTagAssignment(AcceptanceTester $I)
+	{
+		// Setup Plugin.
+		$I->setupConvertKitPlugin($I);
+
+		// Go to the Plugin's Settings > General Screen.
+		$I->amOnAdminPage('options-general.php?page=convertkit-mm');
+
+		// Assign tags.
+		$I->selectOption('convertkit-mm-options[convertkit-mapping-1]', $_ENV['CONVERTKIT_API_TAG_NAME']);
+
+		// Click save settings.
+		$I->click('Save Settings');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+
+		// Confirm settings saved.
+		$I->see('Settings saved.');
+		$I->seeOptionIsSelected('convertkit-mm-options[convertkit-mapping-1]', $_ENV['CONVERTKIT_API_TAG_NAME']);
+	}
+
+	/**
+	 * Deactivate and reset Plugin(s) after each test, if the test passes.
+	 * We don't use _after, as this would provide a screenshot of the Plugin
+	 * deactivation and not the true test error.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function _passed(AcceptanceTester $I)
+	{
+		$I->deactivateThirdPartyPlugin($I, 'membermouse-platform');
+		$I->deactivateConvertKitPlugin($I);
+		$I->resetConvertKitPlugin($I);
+	}
+}

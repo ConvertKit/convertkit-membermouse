b444a8df593405af067df96b911e68c03f1f756d
Tim Carr <tim@n7studios.co.uk>
2024-07-02 11:31:35 +0800

    Added test for changing membership level

---
+++ tests/_support/Helper/Acceptance/MemberMouse.php
@@ -0,0 +1,37 @@
+<?php
+namespace Helper\Acceptance;
+
+/**
+ * Helper methods and actions related to the MemberMouse Plugin,
+ * which are then available using $I->{yourFunctionName}.
+ *
+ * @since   1.2.0
+ */
+class MemberMouse extends \Codeception\Module
+{
+	/**
+	 * Helper method to create a membership level.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I     AcceptanceTester.
+	 * @param 	string 			 $name 	Membership Level Name.
+	 * @return 	int 					Membership Level ID.
+	 */
+	public function memberMouseCreateMembershipLevel($I, $name)
+	{
+		return $I->haveInDatabase(
+			'wp_mm_membership_levels',
+			[
+				'reference_key' => 'm9BvU2',
+				'is_free' => 1,
+				'is_default' => 0,
+				'name' => $name,
+				'description' => $name,
+				'wp_role' => 'mm-ignore-role',
+				'default_product_id' => 0,
+				'status' => 1,
+			]
+		);
+	}
+}
--- tests/acceptance/general/MemberTagCest.php
+++ tests/acceptance/general/MemberTagCest.php
@@ -22,14 +22,14 @@ class MemberTagCest
 	}
 
 	/**
-	 * Test that saving settings on the settings screen with no changes
-	 * works with no errors.
+	 * Test that the member is tagged with the configured "apply tag on add"
+	 * setting when added to a Membership Level.
 	 *
 	 * @since   1.2.0
 	 *
 	 * @param   AcceptanceTester $I  Tester.
 	 */
-	public function testUserSubscribedAndTagged(AcceptanceTester $I)
+	public function testMemberTaggedWhenMembershipLevelAdded(AcceptanceTester $I)
 	{
 		// Setup Plugin to tag users added to the Free Membership level to the
 		// ConvertKit Tag ID.
@@ -68,6 +68,100 @@ class MemberTagCest
 
 		// Check that the subscriber has been assigned to the tag.
 		$I->apiCheckSubscriberHasTag($I, $subscriberID, $_ENV['CONVERTKIT_API_TAG_ID']);
+	}
+
+	/**
+	 * Test that the member is tagged with the configured "apply tag"
+	 * setting when the Membership Level is changed.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testMemberTaggedWhenMembershipLevelChanged(AcceptanceTester $I)
+	{
+		// Create an additional membership level.
+		$levelID = $I->memberMouseCreateMembershipLevel($I, 'Premium');
+
+		// Setup Plugin to tag users added to the Free Membership level to the
+		// ConvertKit Tag ID.
+		$I->setupConvertKitPlugin(
+			$I,
+			[
+				'convertkit-mapping-'.$levelID => $_ENV['CONVERTKIT_API_TAG_ID'],
+			]
+		);
+
+		// Generate email address for test.
+		$emailAddress = $I->generateEmailAddress();
+
+		// Navigate to MemberMouse > Manage Members.
+		$I->amOnAdminPage('admin.php?page=manage_members');
+
+		// Create Member.
+		$I->click('Create Member');
+		$I->waitForElementVisible('#mm-new-member-form-container');
+		$I->fillField('#mm-new-first-name', 'First');
+		$I->fillField('#mm-new-last-name', 'Last');
+		$I->fillField('#mm-new-email', $emailAddress);
+		$I->fillField('#mm-new-password', '12345678');
+		$I->click('Create Member', '.mm-dialog-button-container');
+		$I->waitForElementNotVisible('#mm-new-member-form-container');
+
+		// Accept popup once user created.
+		// We have to wait as there's no specific event MemberMouse fires to tell
+		// us it completed adding the member.
+		$I->wait(3);
+		$I->acceptPopup();
+		$I->wait(3);
+
+		// Change the user's membership level.
+		$I->amOnAdminPage('admin.php?page=manage_members');
+		$I->click($emailAddress);
+		$I->click('Access Rights');
+		$I->selectOption('#mm-new-membership-selection', 'Premium');
+		$I->click('Change Membership');
+
+		// Accept popups
+		// We have to wait as there's no specific event MemberMouse fires to tell
+		// us it completed changing the membership level.
+		$I->wait(3);
+		$I->acceptPopup();
+		$I->wait(3);
+		$I->acceptPopup();
+		$I->wait(3);
+
+		// Check subscriber exists.
+		$subscriberID = $I->apiCheckSubscriberExists($I, $emailAddress);
+
+		// Check that the subscriber has been assigned to the tag.
+		$I->apiCheckSubscriberHasTag($I, $subscriberID, $_ENV['CONVERTKIT_API_TAG_ID']);
+	}
+
+	/**
+	 * Test that the member is tagged with the configured "apply tag on cancelled"
+	 * setting when cancelled.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testMemberTaggedWhenMembershipLevelCancelled(AcceptanceTester $I)
+	{
+		
+	}
+
+	/**
+	 * Test that the member is tagged with the configured "apply tag on cancelled"
+	 * setting when deleted.
+	 *
+	 * @since   1.2.0
+	 *
+	 * @param   AcceptanceTester $I  Tester.
+	 */
+	public function testMemberTaggedWhenDeleted(AcceptanceTester $I)
+	{
+		
 	}
 
 	/**

a2351119234c15bbae6e6dcdd9da139245dc9c67
Tim Carr <tim@n7studios.co.uk>
2024-08-07 15:06:18 +0800

    Fix resource API instance

--- includes/class-convertkit-mm-resource.php
+++ includes/class-convertkit-mm-resource.php
@@ -18,13 +18,20 @@ class ConvertKit_MM_Resource extends ConvertKit_Resource_V4 {
 	 * Constructor.
 	 *
 	 * @since   1.3.0
-	 *
-	 * @param   ConvertKit_MM_API $api_instance   API Instance.
 	 */
-	public function __construct( $api_instance ) {
+	public function __construct() {
 
-		// Initialize the API using the supplied ConvertKit_MM_API instance.
-		$this->api = $api_instance;
+		// Initialize the API if the Access Token has been defined in the Plugin Settings.
+		$settings = new ConvertKit_MM_Settings();
+		if ( $settings->has_access_and_refresh_token() ) {
+			$this->api = new ConvertKit_API_V4(
+				CONVERTKIT_MM_OAUTH_CLIENT_ID,
+				CONVERTKIT_MM_OAUTH_CLIENT_REDIRECT_URI,
+				$settings->get_access_token(),
+				$settings->get_refresh_token(),
+				$settings->debug_enabled()
+			);
+		}
 
 		// Call parent initialization function.
 		parent::init();
